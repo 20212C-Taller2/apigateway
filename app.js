f955a2fba1ce1e6b1cf148f71cefd7c1074f6c93
Nico Gatti <gatti2602@gmail.com>
2021-11-14 10:41:08 -0300

    Added swagger support (openapi v3.0.1)

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const swaggerUi = require('swagger-ui-express'),
+    swaggerDocument = require('./swagger.json');
 
 var router = require('./routes/router')
-
 var app = express();
+
+app.use(
+  '/api-docs',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument)
+);
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
