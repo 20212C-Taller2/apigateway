62a4c9f2d78a2f703bdb61cd048af89a0f4e8962
Nico Gatti <gatti2602@gmail.com>
2021-12-06 09:50:30 -0300

    Added subscriptions endpoints

--- routes/subscriptions.js
+++ routes/subscriptions.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const api = require('./apiAdapter')
 const middleware = require("./middleware");
-const BASE_URL = 'https://ubademy-subscriptions-api.herokuapp.com/' //TODO: Definir url en env var
+const BASE_URL = 'https://ubademy-subscriptions-api.herokuapp.com' //TODO: Definir url en env var
 
 const pass_to_subscriptions_api = (req, res, base = "") => {
   api.run_axios(req, res, BASE_URL + base);
@@ -11,6 +11,21 @@ const pass_to_subscriptions_api = (req, res, base = "") => {
 router.get('/', middleware.validUser, function (req, res, next) {
   pass_to_subscriptions_api(req, res, "subscriptions");
 });
+
+
+router.post('/subscribers', middleware.validUser, function (req, res, next) {
+  pass_to_subscriptions_api(req, res);
+});
+
+
+router.get('/subscribers/:id', middleware.validUser, function (req, res, next) {
+  pass_to_subscriptions_api(req, res);
+});
+
+router.post('subscribers/:id/subscription', middleware.validUser, function (req, res, next) {
+  pass_to_subscriptions_api(req, res);
+});
+
 
 module.exports = {
   router,
