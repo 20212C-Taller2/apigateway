9161d8bd3d7aed2d52ab473be0575a512262851d
Nico Gatti <gatti2602@gmail.com>
2021-12-15 08:16:57 -0300

    Remove dev server

--- bin/push_heroku.sh
+++ bin/push_heroku.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-heroku container:push web -a ubademy-apigateway-dev
-heroku container:release web -a ubademy-apigateway-dev
 
 # PROD
 heroku container:push web -a ubademy-apigateway
